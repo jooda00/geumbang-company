d3d8174741c9266b60c6c18d56537bee6b6092ad
jooda00 <dragon2000a@gmail.com>
2024-09-08 20:35:58 +0900

    feat: first commit

---
+++ src/main/java/com/geumbang/geumbang/GeumbangApplication.java
@@ -0,0 +1,13 @@
+package com.geumbang.geumbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeumbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeumbangApplication.class, args);
+	}
+
+}
